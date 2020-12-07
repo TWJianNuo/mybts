ce90ba9a128ee7de0a47aaca3b6b8b2d00a600ca
TWJianNuo <twjiannuo@gmail.com>
2020-12-06 19:11:48 -0500

    add new exp

--- Exp_ShapeDepth_baseline/train_SDNet_baseline.py
+++ Exp_ShapeDepth_baseline/train_SDNet_baseline.py
@@ -412,9 +412,9 @@ def main_worker(gpu, ngpus_per_node, args):
                 time.sleep(0.1)
                 model.eval()
                 eval_measures_shape, eval_measures_depth = online_eval(model, normoptizer_eval, dataloader_eval, gpu, ngpus_per_node)
-                eval_summary_writer_Shape.add_scalar('L1Measure', eval_measures_shape[0], int(global_step))
-                eval_summary_writer_Depth.add_scalar('Depth_absrel', eval_measures_depth[1], int(global_step))
-                eval_summary_writer_Depth.add_scalar('Depth_a1', eval_measures_depth[6], int(global_step))
+                eval_summary_writer_Shape.add_scalar('L1Measure_semidense', eval_measures_shape[0], int(global_step))
+                eval_summary_writer_Depth.add_scalar('Depth_absrel_semidense', eval_measures_depth[1], int(global_step))
+                eval_summary_writer_Depth.add_scalar('Depth_a1_semidense', eval_measures_depth[6], int(global_step))
 
                 if epoch >= 10:
                     for kk in range(best_measures.shape[0]):
