892ab366ff0b2a4f7aed88ffa402caf214b5ef0c
TWJianNuo <twjiannuo@gmail.com>
2021-08-24 11:53:23 -0400

    add odom depth generation

--- RAFT_tools/export_depth_pred_odom.py
+++ RAFT_tools/export_depth_pred_odom.py
@@ -356,9 +356,9 @@ if __name__ == '__main__':
     if args.evalonly:
         evaluation()
     else:
-        # model = BtsModel(params=args)
-        # train_entries = read_splits(args, istrain=True)
-        # mp.spawn(export, nprocs=ngpus_per_node, args=(model, args, ngpus_per_node, remove_dup(train_entries), True))
-        # eval_entries = read_splits(args, istrain=False)
-        # mp.spawn(export, nprocs=ngpus_per_node, args=(model, args, ngpus_per_node, remove_dup(eval_entries), False))
+        model = BtsModel(params=args)
+        train_entries = read_splits(args, istrain=True)
+        mp.spawn(export, nprocs=ngpus_per_node, args=(model, args, ngpus_per_node, remove_dup(train_entries), True))
+        eval_entries = read_splits(args, istrain=False)
+        mp.spawn(export, nprocs=ngpus_per_node, args=(model, args, ngpus_per_node, remove_dup(eval_entries), False))
         evaluation()
